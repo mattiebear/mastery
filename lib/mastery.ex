68a4662cd680c2db3885a092a5bb2f8f1eb5b025
Matthew Sells <matthew.sells@proton.me>
2025-04-22 16:12:57 -0500

    Remove hello world code and add question test

--- lib/mastery.ex
+++ lib/mastery.ex
@@ -1,18 +1,2 @@
 defmodule Mastery do
-  @moduledoc """
-  Documentation for `Mastery`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Mastery.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
